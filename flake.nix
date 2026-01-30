57ea2a0184166cbce34b62cdf29cca76c590eaac
John Lotoski <john.lotoski@iohk.io>
2026-01-29 19:43:56 -0600

    lazy: add lock file and use lazy for tree-sitter mgmt

--- flake.nix
+++ flake.nix
@@ -188,13 +188,35 @@
     # For advanced custom lua config
     initCustom = pkgs.writeText "init-custom.lua" ''
       print("DEBUG: custom/init.lua is loading!")
-      local ts_bundle = os.getenv("TS_BUNDLE")
-
-      if ts_bundle and vim.uv.fs_stat(ts_bundle) then
-        vim.opt.rtp:prepend(ts_bundle)
-      end
 
       print("DEBUG: end")
+    '';
+
+    lazyLock = builtins.toFile "lazy-lock.json" ''
+      {
+        "LuaSnip": { "branch": "master", "commit": "dae4f5aaa3574bd0c2b9dd20fb9542a02c10471c" },
+        "NvChad": { "branch": "v2.5", "commit": "c57b82473b821274f6017eb03582ba1d13be9d8c" },
+        "base46": { "branch": "v3.0", "commit": "884b990dcdbe07520a0892da6ba3e8d202b46337" },
+        "blink.cmp": { "branch": "main", "commit": "b19413d214068f316c78978b08264ed1c41830ec" },
+        "conform.nvim": { "branch": "master", "commit": "c2526f1cde528a66e086ab1668e996d162c75f4f" },
+        "friendly-snippets": { "branch": "main", "commit": "6cd7280adead7f586db6fccbd15d2cac7e2188b9" },
+        "gitsigns.nvim": { "branch": "main", "commit": "abf82a65f185bd54adc0679f74b7d6e1ada690c9" },
+        "indent-blankline.nvim": { "branch": "master", "commit": "005b56001b2cb30bfa61b7986bc50657816ba4ba" },
+        "lazy.nvim": { "branch": "main", "commit": "306a05526ada86a7b30af95c5cc81ffba93fef97" },
+        "mason.nvim": { "branch": "main", "commit": "44d1e90e1f66e077268191e3ee9d2ac97cc18e65" },
+        "menu": { "branch": "main", "commit": "7a0a4a2896b715c066cfbe320bdc048091874cc6" },
+        "minty": { "branch": "main", "commit": "aafc9e8e0afe6bf57580858a2849578d8d8db9e0" },
+        "nvim-autopairs": { "branch": "master", "commit": "007047febaa3681a8d2f3dd5126fdb9c6e81f393" },
+        "nvim-lspconfig": { "branch": "master", "commit": "79c9a15be5731bc8694840a8fb0c9141c20a80c0" },
+        "nvim-tree.lua": { "branch": "master", "commit": "c07ce43527e5f0242121f4eb1feb7ac0ecea8275" },
+        "nvim-treesitter": { "branch": "main", "commit": "19c729dae6e0eeb79423df0cf37780aa9a7cc3b7" },
+        "nvim-web-devicons": { "branch": "master", "commit": "803353450c374192393f5387b6a0176d0972b848" },
+        "plenary.nvim": { "branch": "master", "commit": "b9fd5226c2f76c951fc8ed5923d85e4de065e509" },
+        "telescope.nvim": { "branch": "master", "commit": "ad7d9580338354ccc136e5b8f0aa4f880434dcdc" },
+        "ui": { "branch": "v3.0", "commit": "cb75908a86720172594b30de147272c1b3a7f452" },
+        "volt": { "branch": "main", "commit": "620de1321f275ec9d80028c68d1b88b409c0c8b1" },
+        "which-key.nvim": { "branch": "main", "commit": "3aab2147e74890957785941f0c1ad87d0a44c15a" }
+      }
     '';
 
     # For available LSPs, view:
@@ -275,12 +297,10 @@
       shellcheck                               # Shell
       statix                                   # Nix
     ];
-
-    tsBundle = pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
-      p.nu
-      p.haskell
-    ]);
   in {
+
+
+
     packages.${system} = rec {
       default = nix-nvchad;
 
@@ -302,6 +322,7 @@
           fd
 
           # Enable compiling additions language grammars on demand from source life
+          # The tree-sitter version will need to be compatible with the lazy-lock.json pin.
           tree-sitter
 
         ];
@@ -319,9 +340,6 @@
 
           # Export the nvim namespacing
           export NVIM_APPNAME=${appName}
-
-          # Export the treesitter grammar tsBundle
-          export TS_BUNDLE="${tsBundle}"
 
           CONFIG_DIR="''${XDG_CONFIG_HOME:-$HOME/.config}/${appName}"
           if [ ! -e "$CONFIG_DIR/init.lua" ]; then
@@ -347,6 +365,9 @@
 
             # Copy plugins init
             cp ${initPlugins} "$CONFIG_DIR"/lua/plugins/init.lua
+
+            # Copy Lazy plugin lock file
+            cp ${lazyLock} "$CONFIG_DIR"/lazy-lock.json
 
             # Copy lspconfig
             cp ${lspConfig} "$CONFIG_DIR"/lua/configs/lspconfig.lua
